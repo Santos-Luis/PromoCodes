68b2ac092094e250ac8b7b96bf5c4266d9b38fb8
Luís Henriques <luis.henriques@uniplaces.com>
2019-08-06 20:36:51 +0100

    Add Elastic Beanstalk configuration

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HealthCheckController extends AbstractController
+{
+    /**
+     * @Route("/health/check", name="health_check")
+     */
+    public function health(): Response
+    {
+        return Response::create();
+    }
+}
